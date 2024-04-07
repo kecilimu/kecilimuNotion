597fdea033473432b425d7b2cf579c7e205f3c2b
kecilimu <87306020+kecilimu@users.noreply.github.com>
2024-04-07 20:15:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -254,7 +254,7 @@ const BLOG = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
     process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-    'https://github.com/imuncle/live2d/blob/master/live2d_3/model/Azue%20Lane(JP)/tianlangxing_3/tianlangxing_3.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+    'https://raw.githubusercontent.com/imuncle/live2d/master/model/22_high/model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
