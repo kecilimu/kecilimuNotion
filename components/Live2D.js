18177625634497e8a167af9118ea5031fc104b96
kecilimu <87306020+kecilimu@users.noreply.github.com>
2024-04-08 18:43:14 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -3,6 +3,7 @@ import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
 import { isMobile, loadExternalResource } from '@/lib/utils'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 /**
  * 网页动画
@@ -16,22 +17,8 @@ export default function Live2D() {
   useEffect(() => {
     if (showPet && !isMobile()) {
       Promise.all([
-        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js'),
-        loadExternalResource('https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js', 'js'),
-        loadExternalResource('https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js', 'js'),
-        loadExternalResource('https://cdn.jsdelivr.net/npm/live2dv3@1.2.2/live2dv3.min.js', 'js'),
-        loadExternalResource('https://raw.githubusercontent.com/HCLonely/Live2dV3/master/live2dv3.js', 'js')
-        
+        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
       ]).then((e) => {
-         l2dv = new l2dViewer({
-            el: document.getElementById('live2d'),
-            basePath: 'https://cdn.jsdelivr.net/npm/live2dv3@latest/assets',
-            modelName: 'biaoqiang_3',
-            sounds: [
-                'sounds/demo.mp3', // 相对路径是相对于模型文件夹
-                'https://cdn.jsdelivr.net/npm/live2dv3@latest/assets/biaoqiang_3/sounds/demo.mp3' // 也可以是网址
-            ]
-        })
         if (typeof window?.loadlive2d !== 'undefined') {
           // https://github.com/xiazeyu/live2d-widget-models
           try {
@@ -55,9 +42,28 @@ export default function Live2D() {
     return <></>
   }
 
-  return <canvas id="live2d" width="400" height="450" onClick={handleClick}
+  return (<canvas id="live2d" width="400" height="450" onClick={handleClick}
         className="cursor-grab"
         onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
         onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
     />
+      <Script src="https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js">
+      <Script src="https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js">
+      <Script src="https://cdn.jsdelivr.net/npm/live2dv3@1.2.2/live2dv3.min.js">
+      <Script src='https://raw.githubusercontent.com/HCLonely/Live2dV3/master/live2dv3.js'>
+     <Script
+        onLoad={() => {
+          new l2dViewer({
+            el: document.getElementById('L2dCanvas'),
+            basePath: 'https://cdn.jsdelivr.net/npm/live2dv3@latest/assets',
+            modelName: 'biaoqiang_3',
+            sounds: [
+                'sounds/demo.mp3', // 相对路径是相对于模型文件夹
+                'https://cdn.jsdelivr.net/npm/live2dv3@latest/assets/biaoqiang_3/sounds/demo.mp3' // 也可以是网址
+            ]
+        })
+        }}
+      />
+    </>     
+  )
 }
