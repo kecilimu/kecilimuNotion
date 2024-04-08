6596b5a80430fe743624527b14ceeb773358c5c9
kecilimu <87306020+kecilimu@users.noreply.github.com>
2024-04-08 20:08:09 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -13,6 +13,23 @@ export default function Live2D() {
   const { theme, switchTheme } = useGlobal()
   const showPet = JSON.parse(siteConfig('WIDGET_PET'))
   const petLink = siteConfig('WIDGET_PET_LINK')
+    useEffect(() => {
+   if (showPet && !isMobile()) {
+      Promise.all([
+        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
+      ]).then((e) => {
+        if (typeof window?.loadlive2d !== 'undefined') {
+          // https://github.com/xiazeyu/live2d-widget-models
+          try {
+            loadlive2d('live2d', petLink)
+          } catch (error) {
+            console.error('读取PET模型', error)
+          }
+        }
+       
+      })
+    }
+ }, [theme])
 /**
 *  useEffect(() => {
 *    if (showPet && !isMobile()) {
